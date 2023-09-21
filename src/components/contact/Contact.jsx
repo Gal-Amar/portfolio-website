eb05c2335061aa1331be553db12e1690744073b6
Gal-Amar <galamar22@gmail.com>
2023-09-21 12:00:12 +0300

    cv update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -74,7 +74,7 @@ const Contact = () => {
           <input type='text' name='name' placeholder='Your Full Name'  required />
           <input type='email' name='email' placeholder='Your Email'  required />
           <textarea name='message' rows={7} placeholder='Your Message' onChange={handleInputChange} value={inputText} required></textarea>
-          <button type='submit' className='btn btn-primary'>Send Messge</button>
+          <button type='submit' className='btn btn-primary b'>Send Messge</button>
           {message && <div className="message">{message}</div>}
         </form>
       </div>
