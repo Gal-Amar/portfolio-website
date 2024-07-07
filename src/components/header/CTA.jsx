29f590c2286d8881f46a8860e808a5ec9d9b0415
Gal-Amar <galamar22@gmail.com>
2024-07-07 15:14:26 +0300

    Update resume name

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import CV from "../../assets/cv.pdf"
+import React from "react";
+// import CV from "../../assets/cv";
 
 const CTA = () => {
   return (
-    <div className='cta'>
-      <a href={CV} download="Gal-Amar-CV" className='btn'>Download CV</a>
-      <a href="#contact"  className='btn btn-primary'>let's Talk</a>
-       </div>
+    <div className="cta">
+      <a href={"../../assets/cv"} download="Gal-Amar-CV" className="btn">
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        let's Talk
+      </a>
+    </div>
+  );
+};
 
-  )
-}
-
-export default CTA
+export default CTA;
