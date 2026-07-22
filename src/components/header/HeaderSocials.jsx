8990fc1bb38cab95b3b48fd74493c6aa2f4d5eba
Gal-Amar <galamar22@gmail.com>
2026-07-22 22:27:42 +0300

    fixing bug

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,15 +1,23 @@
-
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
 
 const HeaderSocials = () => {
   return (
     <div className="header_socials">
-      <a href="https://www.linkedin.com/in/amar-gal/" target="_blank"><BsLinkedin size={25}/></a>
-      <a href="https://github.com/Gal-Amar" target="_blank"> <FaGithub size={27}/></a>
+      <a
+        href="https://www.linkedin.com/in/amar-gal/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsLinkedin size={25} />
+      </a>
+      <a href="https://github.com/Gal-Amar" target="_blank" rel="noreferrer">
+        {" "}
+        <FaGithub size={27} />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
